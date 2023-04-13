b3547c90471cd910bd91f558d433a791e1363348
LiLeijie <2501639085@qq.com>
2023-04-13 21:36:46 +0800

    Add files via upload
    scTCAP update

---
+++ script/scTCAP_args.py
@@ -0,0 +1,24 @@
+import torch
+
+
+class Config(object):
+    """配置参数"""
+
+    def __init__(self):
+        self.model_name = 'Transformer'
+        self.embedding_pretrained = None  # 预训练词向量
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # 设备
+        self.dropout = 0.5  # 随机失活
+        self.num_classes = 9  # 类别数
+        self.num_epochs = 60  # epoch数
+        self.batch_size = 128  # mini-batch大小
+        self.pad_size = 128  # 每句话处理成的长度(短填长切)
+        self.n_vocab = None  # 这里需要读取数据的部分进行赋值
+        self.learning_rate = 5e-4  # 学习率
+        self.embed = 90  # 词向量维度
+        self.dim_model = 90
+        self.hidden = 1024
+        self.last_hidden = 512
+        self.num_head = 5
+        self.num_encoder = 2
+        self.n_splits = 5  # k折交叉验证
